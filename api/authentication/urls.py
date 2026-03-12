3bd5eae32757085591d68093f458ef17f4ce9ce3
ElianPICQ <picq.elian@gmail.com>
2026-03-12 14:56:11 +0100

    reset password feature

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
 #    path('register/', views.Register.as_view()),
     path('login/', views.Login.as_view()),
     path('logout/', views.Logout.as_view()),
+    
+    path('resetpassword/', views.ResetPassword.as_view()),
 ]
